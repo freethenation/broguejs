ee790f59e1791dd2c1cd3c791bac5c7e3b63f041
Richard Klafter <richard@zinc.com>
2017-08-05 01:08:36 -0700

    I can move!

--- src/platform/javascript-platform.c
+++ src/platform/javascript-platform.c
@@ -14,6 +14,15 @@
 extern playerCharacter rogue;
 
 static void gameLoop() {
+	//register a JS handler to queue mouse & keyboard input
+	EM_ASM(
+		var origin = window.location.origin;
+		window.keyOrMouseEvents = [];
+		window.addEventListener('message', function(e){
+			if(origin!==e.origin) return;
+			window.keyOrMouseEvents.push(e.data);
+		}, false);
+	);
 	rogueMain();
 }
 
@@ -22,20 +31,58 @@ static void javascript_plotChar(uchar ch,
 			  short foreRed, short foreGreen, short foreBlue,
 			  short backRed, short backGreen, short backBlue) {
     EM_ASM_({
-      window.parent.postMessage([$0, $1, $2, $3, $4, $5, $6, $7, $8], "*");
+      window.parent.postMessage([$0, $1, $2, $3, $4, $5, $6, $7, $8], '*');
     }, ch, xLoc, yLoc, foreRed, foreGreen, foreBlue, backRed, backGreen, backBlue);
 }
 
 static boolean javascript_pauseForMilliseconds(short milliseconds) {
     emscripten_sleep(milliseconds);
-    return false;  // TODO: return true if we have key/mouse events
+	return EM_ASM_INT({
+		return Math.min(window.keyOrMouseEvents.length, 1);
+	}, 0);
 }
 
+//Passing structs from JS is tricky. Instead of doing that JS simply calls the function below with the nextKeyOrMouseEvent
+rogueEvent javascript_nextRogueEvent;
+static void EMSCRIPTEN_KEEPALIVE javascript_receiveNextKeyOrMouseEvent(enum eventTypes eventType, signed long param1, signed long param2, boolean controlKey, boolean shiftKey) {
+	if(eventType != NUMBER_OF_EVENT_TYPES){
+		EM_ASM_({console.log($0,$1,$2,$3,$4);},eventType,param1,param2,controlKey,shiftKey);
+	}
+	javascript_nextRogueEvent.eventType = eventType;
+	javascript_nextRogueEvent.param1 = param1;
+	javascript_nextRogueEvent.param2 = param2;
+	javascript_nextRogueEvent.controlKey = controlKey;
+	javascript_nextRogueEvent.shiftKey = shiftKey;
+}
 static void javascript_nextKeyOrMouseEvent(rogueEvent *returnEvent, boolean textInput, boolean colorsDance) {
-    return;
+	if (noMenu && rogue.nextGame == NG_NOTHING) rogue.nextGame = NG_NEW_GAME;
+	for (;;) {
+		if (colorsDance) {
+			shuffleTerrainColors(3, true);
+			commitDraws();
+		}
+		EM_ASM_({
+			var nxt = window.keyOrMouseEvents.shift();
+			if(nxt){
+				_javascript_receiveNextKeyOrMouseEvent.apply(null, nxt);
+			} else {
+				_javascript_receiveNextKeyOrMouseEvent($0, 0, 0, 0, 0);
+			}
+		}, NUMBER_OF_EVENT_TYPES);
+		if(javascript_nextRogueEvent.eventType != NUMBER_OF_EVENT_TYPES){
+			returnEvent->eventType = javascript_nextRogueEvent.eventType;
+			returnEvent->param1 = javascript_nextRogueEvent.param1;
+			returnEvent->param2 = javascript_nextRogueEvent.param2;
+			returnEvent->controlKey = javascript_nextRogueEvent.controlKey;
+			returnEvent->shiftKey = javascript_nextRogueEvent.shiftKey;
+			return;
+		}
+		emscripten_sleep(50);
+	}
 }
 
 static void javascript_remap(const char *input_name, const char *output_name) {
+
 }
 
 static boolean modifier_held(int modifier) {
